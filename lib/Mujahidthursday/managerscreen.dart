00fe176c3ff06412cf8a4422a383c5432e93343d
Arsalan <arsalank28@gmail.com>
2022-06-03 02:08:21 +0530

    ggghhh

--- lib/Mujahidthursday/managerscreen.dart
+++ lib/Mujahidthursday/managerscreen.dart
@@ -17,115 +17,224 @@ class _ManagerScreeenState extends State<ManagerScreeen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Container(
-
-
         child: Scaffold(
-          backgroundColor: kGray3,
-        appBar :  AppBar(
-          title: Text("todaytix",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30,color: kred),),
-            actions: [Padding(
-              padding:   EdgeInsets.only(right: 15,top: 15),
-              child: Stack(children: [Icon(Icons.notifications,color: kblack,size: 30,),
-                CircleAvatar(radius: 6,backgroundColor: kred,)
-              ]),
-            ),
-
-            ],
-
-            centerTitle: true,
-            flexibleSpace: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    colors: [ kgolder2,kgradientYellow,kgolder2]
-                ),
-                  borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(15),
-                bottomRight: Radius.circular(15)),
-              ),
-            ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-              bottom: Radius.circular(15),
-            ),
-          ),
-
-
-          toolbarHeight: 55,
-            elevation: 0,
-          ),
           body: Stack(
-            children: [
+              children: [
+                Container(
 
-              Container(
-    decoration: BoxDecoration(
-    gradient: LinearGradient(
+                  decoration: BoxDecoration(
+                    gradient:LinearGradient(
+                        colors: [
+                          Colors.grey,
+                          Colors.black,
 
-    begin: Alignment.topRight,
-    end: Alignment.bottomLeft,
-    colors: [kGray3, kblack, kGray3,  kblack,]
-    ),)
+                          Colors.grey,
+                          Colors.black,
+                          //add more colors for gradient
+                        ],
+                        begin: Alignment.topCenter, //begin of the gradient color
+                        end: Alignment.bottomCenter, //end of the gradient color
+                        stops: [0, 0.2, 0.6,11] //stops for individual color
+                      //set the stops number equal to numbers of color
+                    ),
 
-    // ),
-    //       child: Column(
-    //         children: [
-    //           Container(
-    //             height: 40,
-    //             width: 40,
-    //             decoration: BoxDecoration(
-    //               gradient: LinearGradient(
-    //                   colors: [ kgolder2,kgradientYellow,kgolder2]
-    //               ),
-    //               borderRadius: BorderRadius.all(Radius.circular(10)),
-    //             ),
-    //
-    //           ),
-    //         ],
-    //       ),
-        ),
-              Center(
+                  ),
 
-                child: Column(
+
+
+                ),
+                Column(
                   children: [
-                    Row(
+                    Container(
+                      height: 60,
+                      width: Get.width,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(15),bottomRight: Radius.circular(15)),
+                        gradient:LinearGradient(
+                            colors: [
+                              kgolder,
+
+
+                              Colors.yellow.shade200,
+                              kgolder,
+                              //add more colors for gradient
+                            ],
+                            begin: Alignment.centerLeft, //begin of the gradient color
+                            end: Alignment.centerRight, //end of the gradient color
+                            stops: [0,  0.6,11] //stops for individual color
+                          //set the stops number equal to numbers of color
+                        ),
+                      ),
+                      child: Padding(
+                        padding:  EdgeInsets.only(left: 80),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Text("todaytix",style: TextStyle(
+                                color: Colors.redAccent[400],
+                                fontSize: 30,
+                                fontWeight: FontWeight.bold
+
+                            ),),
+                            Stack(
+                                children:[
+                                  Icon(Icons.notifications,size: 30,),
+                                  Padding(
+                                    padding:  EdgeInsets.only(left: 17,top: 4),
+                                    child: CircleAvatar(
+                                      radius: 7,
+                                      backgroundColor: Colors.redAccent[400],
+                                      child: Text("54",style: TextStyle(color: Colors.white,fontSize: 8),),
+                                    ),
+                                  )
+                                ])
+                          ],
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                      height: 35,
+                      width: 160,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                        gradient:LinearGradient(
+                            colors: [
+                              kgolder,
+
+
+                              Colors.yellow.shade200,
+                              kgolder,
+                              //add more colors for gradient
+                            ],
+                            begin: Alignment.centerLeft, //begin of the gradient color
+                            end: Alignment.centerRight, //end of the gradient color
+                            stops: [0,  0.6,11] //stops for individual color
+                          //set the stops number equal to numbers of color
+                        ),
+                      ),
+                      child: Center(child: Text("Manager",style: TextStyle(fontSize: 15,fontWeight: FontWeight.bold),)),
+                    ),
+
+                    SizedBox(height: 20),
+                    Column(
                       children: [
-                        Container(
-                          height: 150,
-    width: 150,
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.all(Radius.circular(8)),
-    gradient: LinearGradient(
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            buildContainer("assets/certifcts.png","Certificates"),
+                            buildContainer("assets/report.png","Reports"),
+                          ],
+                        ),
+                        SizedBox(height: 20,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            buildContainer("assets/benchlist.png","Benchlist"),
+                            buildContainer("assets/adminleaves.png","Administrative Leave"),
+                          ],
+                        ),
 
-    begin: Alignment.topCenter,
-    end: Alignment.bottomCenter,
-    colors: [kgradientYellow, kgolder2, kgradientYellow,]
-    ),)
 
-        ),
-                        Container(
-                          height: 150,
-    width: 150,
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.all(Radius.circular(8)),
-    gradient: LinearGradient(
+                        SizedBox(height: 20,),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            buildContainer("assets/leaves.png","Leaves"),
+                            buildContainer("assets/othersrv.png","Other Services"),
+                          ],
+                        ),
 
-    begin: Alignment.topCenter,
-    end: Alignment.bottomCenter,
-    colors: [kgradientYellow, kgolder2, kgradientYellow,]
-    ),)
+                        SizedBox(height: 30),
+                        Padding(
+                          padding:  EdgeInsets.only(left: 295.0),
+                          child: Container(
 
-        ),
+                            width: 50,
+                            height: 50,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.all(Radius.circular(50)),
+                            ),
+                            child: FittedBox(
+                                fit: BoxFit.none,
+                                child: Container(
+
+                                  width: 50,
+                                  height: 50,
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                                    boxShadow: [BoxShadow(color: kgolder,blurRadius: 15)],
+                                    color: kgolder,
+                                  ),
+                                  child: Center(
+                                      child: Image.asset("assets/icon2.png",fit: BoxFit.fitWidth,)),
+                                )
+                            ),
+                          ),
+                        ),
 
                       ],
                     ),
+
+
+
+                    // Container(
+                    //     width:50,
+                    //     height: 50,
+                    //     decoration: BoxDecoration(
+                    //       borderRadius: BorderRadius.all(Radius.circular(50)),
+                    //       color: kgolder,
+                    //       boxShadow:  [ BoxShadow(
+                    //         color: kgolder,
+                    //         blurRadius: 20.0,
+                    //       ),]
+                    //     ),
+                    //     child: FittedBox(child: ClipRRect(child: Image.asset("assets/icon.png",fit: BoxFit.fitWidth,)))),
                   ],
                 ),
-              ),
 
 
-            ]
-          ),),
-      )
+
+              ]
+          ),
+        ),
+      ),
 
     );
   }
+
+  Container buildContainer(String image, String tabname) {
+    return Container(
+      height: 160,
+      width: 160,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.all(Radius.circular(10)),
+        gradient:LinearGradient(
+            colors: [
+              kgolder,
+
+
+              Colors.yellow.shade200,
+              kgolder,
+              //add more colors for gradient
+            ],
+            begin: Alignment.topCenter, //begin of the gradient color
+            end: Alignment.bottomCenter, //end of the gradient color
+            stops: [0,  0.6,11] //stops for individual color
+          //set the stops number equal to numbers of color
+        ),
+      ),
+      child: Padding(
+        padding:  EdgeInsets.only(top: 17),
+        child: Column(
+          children: [
+            Image.asset(image,color: Colors.black),
+            SizedBox(height: 10,),
+            Text(tabname)
+          ],
+        ),
+      ),
+    );
+  }
+
 }
